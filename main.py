7469b2e2534502d77c231ea42ac9b27b0d73f356
Michael Neiman <michaeln.perfetto@gmail.com>
2020-06-07 01:16:59 +0300

    Added a few more stages.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, ConversationHandler)
 import logging
-from gerkules_mongodb import GerkulesMongodb, GerkulesFood
+from gerkules_mongodb import (GerkulesMongodb, GerkulesFood)
 
 bot_token = '1260422753:AAGk_W_MfVoCD-HqTp54cAqTGay34Ll4gs8'
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -8,37 +9,80 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 gerkules_database = GerkulesMongodb()
 
+SET_CALORIES, INPUT_MEAL = range(2)
+
 
 def start(update, context):
     global gerkules_database
-    update.message.reply_text('Привет!')
     user = update.message.from_user
     gerkules_database.add_user(user)
-    logger.info(f"Started conversation with {user.username}")
+
+    logger.info(f"Started conversation with {user.username}.")
+
+    keyboard = [['Start my plan!'], ['Help']]
+    markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+    update.message.reply_text('Hi! What would you like to do?', reply_markup=markup)
+
+    return SET_CALORIES
+
+
+def set_calories(update, context):
+    user = update.message.from_user
+    logger.info(f"{user.username} is inputting their calorie quota.")
+
+    update.message.reply_text('Please tell me your goal. Insert how many calories do you want to consume per day.')
+    if is_string_int(update.message.text):
+        logger.info(update.message.text)
+        return INPUT_MEAL
+
+
+def stop(update, context):
+    update.message.reply_text('Bye!')
+    return ConversationHandler.END
 
 
 def help(update, context):
     update.message.reply_text('Help!')
 
 
-def echo(update, context):
-    update.message.reply_text(update.message.text)
-
-
 def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def input_meal(update, context):
+    user = update.message.from_user
+    update.message.reply_text('What did you eat?')
+    return ConversationHandler.END
+
 
 
 def main():
     updater = Updater(bot_token, use_context=True)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+            SET_CALORIES: [MessageHandler(Filters.text, set_calories)],
+            INPUT_MEAL: [MessageHandler(Filters.text, input_meal)]
+        },
+        fallbacks=[CommandHandler('stop', stop)],
+        name="my_conversation",
+
+    )
+    dp.add_handler(conv_handler)
+
     dp.add_error_handler(error)
     updater.start_polling()
     updater.idle()
 
+
+def is_string_int(input_string):
+    try:
+        int(input_string)
+        return True
+    except ValueError:
+        return False
 
 if __name__ == '__main__':
     main()
